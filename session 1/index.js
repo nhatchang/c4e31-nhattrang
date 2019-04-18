a91db1a973c5560966ab6996c77ce8ffdd04e72a
nhattrang <phntrang145@gmail.com>
2019-04-18 22:18:27 +0700

    lel

---
+++ session 1/index.js
@@ -0,0 +1,24 @@
+// let name = prompt("Enter your name? ");
+// console.log("Hello, name")
+
+// const x = prompt("Enter x");
+// const y = prompt("Enter y");
+
+// console.log(Number(x) + Number(y));
+
+// Steps
+// 1. Tạo file html
+// 2. Gõ html, chọn html:5 để hiển thị khung html
+// 3. Tạo file js để code js
+// 4. Nối 2 file với nhau thông qua script, code ở js
+// 5. Chạy file html
+// 6. Inspect (Kiểm tra) trên trình duyệt
+// 7. Chuyển qua tab Console để xem kết quả
+
+// loops: 
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+    console.log("Hello");
+}
+
+// ++ tăng thêm 1 đơn vị
